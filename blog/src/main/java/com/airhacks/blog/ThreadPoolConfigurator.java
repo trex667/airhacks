43eeebda876da355cfdbca47e735749b4795781f
Adam Bien <abien@adam-bien.com>
2017-11-20 14:01:07 +0100

    overloading use case is working

---
+++ blog/src/main/java/com/airhacks/blog/ThreadPoolConfigurator.java
@@ -0,0 +1,35 @@
+
+package com.airhacks.blog;
+
+import com.airhacks.porcupine.configuration.control.ExecutorConfigurator;
+import com.airhacks.porcupine.execution.control.ExecutorConfiguration;
+import java.util.concurrent.ThreadPoolExecutor;
+import javax.enterprise.inject.Specializes;
+
+/**
+ *
+ * @author airhacks.com
+ */
+@Specializes
+public class ThreadPoolConfigurator extends ExecutorConfigurator {
+
+    @Override
+    public ExecutorConfiguration forPipeline(String name) {
+        if ("mes".equalsIgnoreCase(name)) {
+            return new ExecutorConfiguration.Builder().
+                    callerRunsPolicy().
+                    customRejectedExecutionHandler(this::rejectedExecution).
+                    corePoolSize(1).
+                    maxPoolSize(1).
+                    queueCapacity(1).
+                    build();
+        }
+        return super.forPipeline(name);
+    }
+
+    public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+        System.out.println("-- overloaded: " + r.toString() + " " + executor);
+    }
+
+
+}
