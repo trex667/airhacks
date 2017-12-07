d2dd49a4c657ff35f27cdbcc5781a8f819fadaff
Adam Bien <abien@adam-bien.com>
2017-12-07 11:43:55 +0100

    Custom serialization format implemented

---
+++ blog/src/main/java/com/airhacks/blogg/JsonObjectSerializer.java
@@ -0,0 +1,38 @@
+
+package com.airhacks.blogg;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
+
+/**
+ *
+ * @author airhacks.com
+ */
+@Produces("a/o")
+@Provider
+public class JsonObjectSerializer implements MessageBodyWriter<Object> {
+
+    @Override
+    public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return true;
+    }
+
+    @Override
+    public long getSize(Object t, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return -1;
+    }
+
+    @Override
+    public void writeTo(Object t, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
+        entityStream.write(("serialization test " + t.toString()).getBytes());
+    }
+
+}
