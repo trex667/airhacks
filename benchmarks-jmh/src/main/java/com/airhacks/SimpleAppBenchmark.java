6c3eec8781a62b41e50dcc7b7779140429e3a4f0
Adam Bien <abien@adam-bien.com>
2017-12-13 13:33:17 +0100

    JMH as stress test driver

--- benchmarks-jmh/src/main/java/com/airhacks/SimpleAppBenchmark.java
+++ benchmarks-jmh/src/main/java/com/airhacks/SimpleAppBenchmark.java
@@ -1,7 +1,8 @@
 package com.airhacks;
 
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Logger;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
@@ -19,23 +20,24 @@ import org.openjdk.jmh.annotations.Warmup;
 @Measurement(iterations = 2, time = 1, timeUnit = TimeUnit.SECONDS)
 @BenchmarkMode(Mode.All)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
-public class LoggingBenchmark {
+public class SimpleAppBenchmark {
 
     @State(Scope.Benchmark)
     public static class LoggerState {
 
-        Logger log;
+        WebTarget tut;
 
         @Setup(Level.Trial)
         public void init() {
-            this.log = Logger.getLogger(this.getClass().getName());
+            this.tut = ClientBuilder.newClient().target("http://localhost:8080/simpleapp/resources/ping");
         }
 
     }
 
     @Benchmark
-    public void log(LoggerState state) {
-        state.log.info("test output");
+    public void fetchData(LoggerState state) {
+        String result = state.tut.request().get(String.class);
+        System.out.println("result = " + result);
     }
 
 }
